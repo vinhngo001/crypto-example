a6699019e9155e882ab73bee1cc2e3147b8ed2a8
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-01-02 11:32:10 +0700

    first time with crypto

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ class Block {
         this.prevHash = prevHash;
         this.data = data;
         this.timestamp = new Date();
-        this.hash = '';
+        this.hash = this.caculateHash();
+    }
+    caculateHash(){
+        return hash(this.prevHash + JSON.stringify(this.data) + this.timestamp)
     }
 }
+
+const block = new Block(prevHash='', data={
+    hello: 'Coders Tokyo'
+});
+
+console.log(block)
