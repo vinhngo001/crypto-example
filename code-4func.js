90d023cb8e79bc50cb280cddfbdc7af33bbad35d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-03 21:23:07 +0700

    commit

--- code-4func.js
+++ code-4func.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 // const lizaiPublicKey = fs.readFileSync('./public.pem', 'utf-8'); // Load your private key
 // const lizaiPrivateKey = fs.readFileSync('./private.pem', 'utf-8'); // Load your private key
-const keyToEncrypt = 'super-hyper-mega-secret';
+const keyToEncrypt = 'i want to live again, forever';
 
 function generateKeys() {
     const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
@@ -11,33 +11,35 @@ function generateKeys() {
     });
     const priv = privateKey.export({ format: 'pem', type: 'pkcs1' })
     fs.writeFileSync('./private.pem', priv);
-    const pub = publicKey.export({ type: 'pkcs1',format: 'pem' })
+    const pub = publicKey.export({ type: 'pkcs1', format: 'pem' })
     fs.writeFileSync('./public.pem', pub)
 }
 generateKeys();
 
-let  lizaiPrivateKey = fs.readFileSync('./private.pem').toString()
-const lizaiPublicKey= fs.readFileSync('./public.pem').toString();
+const lizaiPrivateKey = fs.readFileSync('./private.pem').toString();
+const lizaiPublicKey = fs.readFileSync('./public.pem').toString();
 
-const encryptRSA = crypto.publicEncrypt(
-    {
-        key: lizaiPublicKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
-    },
-    Buffer.from(keyToEncrypt)
+const data = "super hyper mega secret"
+
+const encryptedKey = crypto.publicEncrypt(
+	{
+		key: lizaiPublicKey,
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	// We convert the data string to a buffer using `Buffer.from`
+	Buffer.from(data)
+)
+
+console.log("encypted data: ", encryptedKey.toString("base64"));
+// fs.writeFileSync('encrypted.txt', encryptedData);
+const decryptedKey = crypto.privateDecrypt(
+	{
+		key: lizaiPrivateKey,
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	encryptedKey
 );
 
-const encryptedKey = encryptRSA.toString('base64');
-console.log('Encrypted Key:', encryptedKey);
-
-const decryptRSA = crypto.privateDecrypt(
-    {
-        key: lizaiPrivateKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
-    },
-    Buffer.from(encryptedKey)
-);
-
-console.log('Decrypted Key:', decryptRSA.toString('utf-8'));
+console.log("decrypted data: ", decryptedKey.toString())
