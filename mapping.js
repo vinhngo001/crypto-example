d345ef0206165521779d0c4aa47e5ca732879ce0
Henry Ngo <henry.ngo@lizai.co>
2023-12-12 16:51:41 +0700

    [commit] - commit

--- mapping.js
+++ mapping.js
@@ -1,101 +1,68 @@
 const puppeteer = require("puppeteer-core");
-
-// async function extractTextContent(page, selector) {
-//     try {
-//         const textContent = await page.evaluate(el => el.textContent, selector);
-//         return textContent.trim() || null;
-//     } catch (error) {
-//         console.log(error);
-//         throw error;
-//     }
-// }
-
-// async function extractSrcAttribute(page, selector) {
-//     const src = await page.evaluate(el => el.getAttribute('src'), element);
-//     return src || null;
-// }
-
-// async function extractHrefAttribute(page, selector) {
-//     try {
-//         const href = await page.evaluate(el => el.getAttribute('href'), selector);
-//         return href || null;
-//     } catch (error) {
-//         console.log(error);
-//         throw error;
-//     }
-// }
-
-// const mappingFunctions = {
-//     'h1': extractTextContent,
-//     'img': extractSrcAttribute,
-//     'p': extractTextContent,
-//     'a': extractHrefAttribute
-//     // 'TABLE': extractTableContent,
-//     // Add more mappings for other element types
-// };
-
-// async function executeMapping(page, element) {
-//     if (element) {
-//         // console.log(`Processing element `);
-//         // console.log(element);
-
-//         const tagName = await page.evaluate(el => el.tagName.toLowerCase(), element);
-//         console.log(`Tag name: ${tagName}`);
-
-//         const mappingFunction = mappingFunctions[tagName];
-//         if (mappingFunction) {
-//             const result = await mappingFunction(page, element);
-//             console.log({ result });
-//             return result;
-//         } else {
-//             console.log(`No mapping function found for tagName: ${tagName}`);
-//             // Default handling (you can add more default logic if needed)
-//             return await page.evaluate(el => el.textContent, element);
-//         }
-//     } else {
-//         // Handle the case where element is undefined
-//         console.log('Invalid element:', element);
-//         return null;
-//     }
-// }
+const fs = require('fs')
+const { parse } = require('himalaya');
+const html = fs.readFileSync('content/contentHtml.txt', { encoding: 'utf-8' });
+// console.log({html});
+// return 
+const json = parse(html);
+// fs.writeFileSync('content/finalHTML.json', JSON.stringify(json, null, 2));
+// console.log('👉', json);
 
 const textTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'em', 'abbr', 'acronym', 'address', 'bdo', 'blockquote', 'cite', 'q', 'code', 'ins', 'del', 'dfn', 'kbd', 'pre', 'samp', 'var', 'br'];
 const linkTags = ["a", "base"];
-const imageTags = ["img", "area", "map", "param", "object"];
+const imageTags = ["img", "area", "map", "param", "object", "iframe"];
 const listTags = ["ul", "ol", "li", "dl", "dt", "dd"];
 const tableTags = ["table", "tr", "td", "th", "tbody", "thead", "tfoot", "col", "colgroup", "caption"];
 const scripTags = ["script", "noscript"];
-const formTags = ["form", "input", "textarea", "select", "option", "outgroup", "button", "label", "fieldset", "legend"];
+const formTags = ["form", "input", "textarea", "select", "option", "optgroup", "button", "label", "fieldset", "legend"];
 
 const tagMappings = {
     ...createMapping(textTags, extractTextContent),
-    ...createMapping(linkTags, extractLinkData),
-    ...createMapping(imageTags, extractImageData),
-    ...createMapping(formTags, extractFormData),
-    ...createMapping(listTags, extractListData)
+    // ...createMapping(linkTags, extractLinkData),
+    // ...createMapping(imageTags, extractImageData),
+    // ...createMapping(formTags, extractFormData),
+    // ...createMapping(scripTags, extractFormData),
+    ...createMapping(listTags, extractTextContent)
 }
 
 async function extractTextContent(page, element) {
+    const tagName = await page.evaluate(el => el.tagName, element);
     const textContent = await page.evaluate(el => el.textContent, element);
-    return textContent;
+    return { [tagName.toLowerCase()]: textContent };
 }
 
 async function extractFormData(page, element) {
+    try {
+        const extractedData = {};
+        const content = await page.evaluate(el => el.value || el.textContent, element);
 
-}
+        if (!!content) {
+            const elementId = await page.evaluate(el => el.id || el.name || el.tagName, element);
+            const elementName = elementId.toLowerCase();
+            if (!extractedData[elementName]) {
+                extractedData[elementName] = {};
+            }
+            extractedData[elementName] = content.trim();
+        }
 
-async function extractListData(page, element){
-
+        return extractedData;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function extractLinkData(page, element) {
+    const tagName = await page.evaluate(el => el.tagName, element);
     const href = await page.evaluate(el => el.href, element);
-    return href;
+    // return href;
+    return { [tagName.toLowerCase()]: href };
 }
 
-async function extractImageData(page, element){
+async function extractImageData(page, element) {
+    const tagName = await page.evaluate(el => el.tagName, element);
     const src = await page.evaluate(el => el.src, element);
-    return src;
+    // return src;
+    return { [tagName.toLowerCase()]: src };
 }
 
 function createMapping(tags, extractionFunction) {
@@ -109,7 +76,6 @@ async function executeMapping(page, element) {
     if (element) {
         console.log(`Processing element `);
 
-        // const tagName = element.tagName.toLowerCase();
         const tagName = await page.evaluate(el => el.tagName.toLowerCase(), element);
         console.log(`Tag name: ${tagName}`);
 
@@ -117,7 +83,7 @@ async function executeMapping(page, element) {
         if (extractionFunction) {
             const result = await extractionFunction(page, element);
             return result;
-        }else{
+        } else {
             console.log(`No mapping function found for tagName: ${tagName}`);
         }
     }
@@ -125,7 +91,30 @@ async function executeMapping(page, element) {
     return null;
 }
 
-(async () => {
+function mapData(element) {
+    // Check if the element has text content
+    if (element.type === 'text' && element.content.trim() !== '') {
+        return element.content.trim();
+    }
+
+    // Check if the element has children
+    if (element.children && element.children.length > 0) {
+        const mappedChildren = element.children.map(mapData);
+
+        // Join the results if there is text content
+        const textContent = mappedChildren.join('').trim();
+        if (textContent !== '') {
+            return textContent;
+        }
+    }
+
+    return '';
+}
+
+// const result = mapData(json[0]);
+// console.log({result});
+
+async function main(contentHtml) {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox',],
         headless: true,
@@ -134,7 +123,9 @@ async function executeMapping(page, element) {
     });
     try {
         const page = await browser.newPage();
-        await page.goto("https://www.fda.gov/news-events/press-announcements/fda-approves-first-vaccine-prevent-disease-caused-chikungunya-virus", { waitUntil: ['networkidle2', 'domcontentloaded'], timeout: 20000 });
+        await page.goto("https://www.pdr.net/browse-by-drug-name?search=Aspirin", { waitUntil: ['networkidle2', 'domcontentloaded'], timeout: 20000 });
+
+        // await page.setContent(contentHtml, { waitUntil: ['networkidle2', 'domcontentloaded'], timeout: 20000 });
 
         // Get all elements in the body
         const allElements = await page.$$('body *');
@@ -145,7 +136,8 @@ async function executeMapping(page, element) {
         );
 
         // Log the results
-        console.log('All Elements:', elementData);
+        // console.log('All Elements:', elementData);
+        fs.writeFileSync('content/initialHTML.json', JSON.stringify(elementData.filter(el => el !== null && Object.keys(el).length > 0), null, 2));
         return await browser.close();
     } catch (error) {
         console.log(error);
@@ -153,4 +145,6 @@ async function executeMapping(page, element) {
             await browser.close();
         }
     }
-})();
+}
+
+main(html);
